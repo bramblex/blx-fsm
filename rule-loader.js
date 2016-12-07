aedfa701290d5dc93244fa0ef566cf5c1713ed73
qiaojian <qiaojian@wolfkill.cc>
2016-12-07 17:48:33 +0800

    v0.0.7 fix loader bug

--- rule-loader.js
+++ rule-loader.js
@@ -4,7 +4,20 @@
 'use strict'
 
 const parser = require('./parser')
+const json = JSON.stringify
 
 module.exports = source => {
-  return `module.exports = ${JSON.stringify(parser.parse(source))}`
+  const {define, start, body} = parser.parse(source)
+  return `
+  module.exports = {
+    define: ${json(define)},
+    start: ${json(start)}, 
+    body: [${body.map(([first, input, second]) => {
+      if (first instanceof RegExp) {
+        return `[${first.toString()}, ${json(input)}, ${json(second)}]`
+      } else {
+        return `[${json(first)}, ${json(input)}, ${json(second)}]`
+      }
+    }).join(', ')}]
+  }`
 }
